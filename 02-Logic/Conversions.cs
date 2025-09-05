ed7fa320491ffb92069c0c0ae93eeebbc7371f06
Sergio Barreras <sergiobg.isc@gmail.com>
2025-09-05 17:31:29 -0600

    Implement Conversions method to demonstrate various number conversions

---
+++ 02-Logic/Conversions.cs
@@ -0,0 +1,25 @@
+partial class Program
+{
+    static void Conversions()
+    {
+        int number = 42;
+        double decimalNumber = number;
+
+        WriteLine(decimalNumber);
+
+        double anotherDecimal = 47.5;
+        int integerNumber = (int)anotherDecimal;
+        WriteLine(integerNumber);
+
+        // Convert o Parse
+        string text = "123";
+        int parsedNumber = int.Parse(text);
+        WriteLine(parsedNumber);
+
+        double anotherDecimal2 = 50.8;
+        int convertedNumber = Convert.ToInt32(anotherDecimal2);
+        WriteLine(convertedNumber);
+        int castedNumber = (int)anotherDecimal2;
+        WriteLine(castedNumber);
+    }
+}
