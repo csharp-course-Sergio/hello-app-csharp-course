52804f7d2b955021d3202e0ed7310fd1b726c214
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-04 13:06:21 -0600

    Implement task management menu and list tasks functionality

--- 06-TaskMaster/MainTask.cs
+++ 06-TaskMaster/MainTask.cs
@@ -1,33 +1,53 @@
-partial class Program
-{
-    public static void TaskMaster()
-    {
-        ForegroundColor = ConsoleColor.White;
-        WriteLine("-----Menú de Tareas-----");
-        WriteLine("\n1. Listar tareas");
-        WriteLine("2. Añadir tarea");
-        WriteLine("3. Marcar tarea como completada");
-        WriteLine("4. Editar tarea");
-        WriteLine("5. Eliminar tarea");
-        WriteLine("6. Consultar tareas por estado");
-        WriteLine("7. Consultar tarea por descripción");
-        WriteLine("8. Salir");
-        Write("\nSeleccione una opción: ");
+using TaskMaster;
 
-        switch (ReadLine())
+namespace TaskMaster
+{
+
+
+    partial class Program
+    {
+
+        static readonly FileActions<Task> fileActions = new("./06-TaskMaster/task.json");
+        static readonly List<Task> tasks = fileActions.ReadFile();
+        static readonly Queries queries = new(tasks);
+
+        public static void TaskMaster()
         {
-            case "1": break;
-            case "2": break;
-            case "3": break;
-            case "4": break;
-            case "5": break;
-            case "6": break;
-            case "7": break;
-            case "8": break;
-            default:
-                Console.Clear();
-                Console.WriteLine("Opción no válida. Intente de nuevo.");
-                break;
+            bool salir = false;
+
+            while (!salir)
+            {
+                ForegroundColor = ConsoleColor.White;
+                WriteLine("-----Menú de Tareas-----");
+                WriteLine("\n1. Listar tareas");
+                WriteLine("2. Añadir tarea");
+                WriteLine("3. Marcar tarea como completada");
+                WriteLine("4. Editar tarea");
+                WriteLine("5. Eliminar tarea");
+                WriteLine("6. Consultar tareas por estado");
+                WriteLine("7. Consultar tarea por descripción");
+                WriteLine("8. Salir");
+                Write("\nSeleccione una opción: ");
+
+                switch (ReadLine())
+                {
+                    case "1": queries.ListTasks(); break;
+                    case "2": break;
+                    case "3": break;
+                    case "4": break;
+                    case "5": break;
+                    case "6": break;
+                    case "7": break;
+                    case "8":
+                        salir = true;
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+            }
         }
     }
 }
--- 06-TaskMaster/Queries.cs
+++ 06-TaskMaster/Queries.cs
@@ -3,5 +3,21 @@ namespace TaskMaster
     public class Queries(List<Task> _tasks)
     {
         private readonly List<Task> Tasks = _tasks;
+
+        public void ListTasks()
+        {
+            ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("-----Lista de Tareas-----\n");
+
+            Console.WriteLine("\n{0,-8} {1,-35} {2,-15}", "Id", "Descripción", "Completado");
+
+            foreach (var task in Tasks)
+            {
+
+                WriteLine(new string('-', 58));
+                Console.WriteLine("{0,-8} {1,-35} {2,-15}", task.Id, task.Description, task.Completed);
+            }
+            WriteLine(new string('-', 58));
+        }
     }
 }
